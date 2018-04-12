f9bdaecdc2c14026fafdab4ab4e674f9b19614f9
Crystal Wang <crystalwang95@icloud.com>
2018-04-12 14:02:10 -0500

    added picture, some styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,38 @@
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stick()};
 window.onresize = function() {resize()};
 
-var navbar = document.getElementById("navigationBar");
+/*--- STICKY SIDEBAR --*/
+
+// var headerBottom is invisible div at bottom of header
+// Grab it to find offset position of the sidebar nav
+var headerBottom = document.getElementById("headerBottom");
 var sidebar = document.getElementById("sidebar");
 var content = document.getElementById("content");
-var sticky = navbar.offsetTop;
+
+// Sticky is the position at which we want the sidebar to be stickied
+var sticky = headerBottom.offsetTop;
+
+// Keep track of width bc fixed will resize relative to viewport
 var width = sidebar.offsetWidth;
 
+// Adjust fixed position on resize
 function resize(){
-    sticky = navbar.offsetTop;
-    width = sidebar.offsetWidth;
+    // Update sticky height
+    sticky = headerBottom.offsetTop;
+
+    // Width will be 1/4 content width as it takes up 3 cols
+    width = content.offsetWidth/4;
+    // Call to stick
+    stick();
 }
 
-function myFunction() {
-  if (window.pageYOffset >= sticky+53) {
-    sidebar.classList.add("stickySide");
+function stick() {
+  sticky = headerBottom.offsetTop;
+  if (window.pageYOffset >= sticky) {
+    sidebar.classList.add("sticky");
     sidebar.style.width = width+"px";
-
   } else {
-    sidebar.classList.remove("stickySide");
+    sidebar.classList.remove("sticky");
   }
 }
