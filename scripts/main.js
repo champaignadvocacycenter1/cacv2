b76bdb7d52fb8a6f98aadc4313a4e645607909a9
Crystal Wang <crystalwang95@icloud.com>
2018-04-12 01:41:07 -0500

    stickied

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
 
+window.onscroll = function() {myFunction()};
+window.onresize = function() {resize()};
+
+var navbar = document.getElementById("navigationBar");
+var sidebar = document.getElementById("sidebar");
+var content = document.getElementById("content");
+var sticky = navbar.offsetTop;
+var width = sidebar.offsetWidth;
+
+function resize(){
+    sticky = navbar.offsetTop;
+    width = sidebar.offsetWidth;
+}
+
+function myFunction() {
+  if (window.pageYOffset >= sticky+53) {
+    sidebar.classList.add("stickySide");
+    sidebar.style.width = width+"px";
+
+  } else {
+    sidebar.classList.remove("stickySide");
+  }
+}
