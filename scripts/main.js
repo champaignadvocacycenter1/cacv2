f59683785b3ffb7c29b03092678268d1c4350cd1
Crystal Wang <crystalwang95@icloud.com>
2018-04-11 20:58:52 -0500

    Added side nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,1 @@
 
-window.onscroll = function() {myFunction()};
-
-var header;
-while( header==null){
-    header=document.getElementById("navigation");
-}
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
